cd30aad03c5da7b5564d451ba21c4b43329ed0a4
Nutria.sh <106103733+nutria22@users.noreply.github.com>
2023-01-21 19:44:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LoadingScreen
+ A beautifully designed YouTube video player with particle animations to use as a loading screen on your server.
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+
+description 'FiveM Loading Screen'
+
+files {
+    "config.js",
+    "index.html",
+    "assets/*.*"
+}
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
